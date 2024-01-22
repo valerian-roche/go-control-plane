bc73b797717ebd0b8f22cc801c74af9adcc29468
Valerian Roche <valerian.roche@datadoghq.com>
2024-01-21 23:13:07 -0500

    Add test for resource subscription tracking

--- pkg/server/delta/v3/server.go
+++ pkg/server/delta/v3/server.go
@@ -212,19 +212,22 @@ func (s *server) processDelta(str stream.DeltaStream, reqCh <-chan *discovery.De
 			// cancel existing watch to (re-)request a newer version
 			watch, ok := watches.deltaWatches[typeURL]
 			if !ok {
-				// Initialize the state of the stream.
-				// Since there was no previous state, we know we're handling the first request of this type
+				// Initialize the state of the type subscription.
+				// Since there was no previous subscription, we know we're handling the first request of this type
 				// so we set the initial resource versions if we have any.
-				// We also set the stream as wildcard based on its legacy meaning (no resource name sent in resource_names_subscribe).
-				// If the state starts with this legacy mode, adding new resources will not unsubscribe from wildcard.
+				// We also set the subscription as wildcard based on its legacy meaning (no resource name sent in resource_names_subscribe).
+				// If the subscription starts with this legacy mode, adding new resources will not unsubscribe from wildcard.
 				// It can still be done by explicitly unsubscribing from "*"
-				watch.subscription = stream.NewSubscription(len(req.GetResourceNamesSubscribe()) == 0, req.GetInitialResourceVersions())
+				watch.subscription = stream.NewDeltaSubscription(req.GetResourceNamesSubscribe(), req.GetResourceNamesUnsubscribe(), req.GetInitialResourceVersions())
 			} else {
 				watch.Cancel()
-			}
 
-			s.subscribe(req.GetResourceNamesSubscribe(), &watch.subscription)
-			s.unsubscribe(req.GetResourceNamesUnsubscribe(), &watch.subscription)
+				// Update subscription with the new requests
+				watch.subscription.UpdateResourceSubscriptions(
+					req.GetResourceNamesSubscribe(),
+					req.GetResourceNamesUnsubscribe(),
+				)
+			}
 
 			var err error
 			watch.cancel, err = s.cache.CreateDeltaWatch(req, watch.subscription, watches.deltaMuxedResponses)
@@ -260,41 +263,4 @@ func (s *server) DeltaStreamHandler(str stream.DeltaStream, typeURL string) erro
 	}()
 
 	return s.processDelta(str, reqCh, typeURL)
-}
-
-// When we subscribe, we just want to make the cache know we are subscribing to a resource.
-// Even if the stream is wildcard, we keep the list of explicitly subscribed resources as the wildcard subscription can be discarded later on.
-func (s *server) subscribe(resources []string, subscription *stream.Subscription) {
-	sv := subscription.SubscribedResources()
-	for _, resource := range resources {
-		if resource == "*" {
-			subscription.SetWildcard(true)
-			continue
-		}
-		sv[resource] = struct{}{}
-	}
-}
-
-// unsubscribe remove resources from the stream's subscribed resource list.
-// If a client explicitly unsubscribes from a wildcard request, the stream is updated and now watches only subscribed resources.
-func (s *server) unsubscribe(resources []string, subscription *stream.Subscription) {
-	sv := subscription.SubscribedResources()
-	for _, resource := range resources {
-		if resource == "*" {
-			subscription.SetWildcard(false)
-			continue
-		}
-		if _, ok := sv[resource]; ok && subscription.IsWildcard() {
-			// The XDS protocol states that:
-			// * if a watch is currently wildcard
-			// * a resource is explicitly unsubscribed by name
-			// Then the control-plane must return in the response whether the resource is removed (if no longer present for this node)
-			// or still existing. In the latter case the entire resource must be returned, same as if it had been created or updated
-			// To achieve that, we mark the resource as having been returned with an empty version. While creating the response, the cache will either:
-			// * detect the version change, and return the resource (as an update)
-			// * detect the resource deletion, and set it as removed in the response
-			subscription.ReturnedResources()[resource] = ""
-		}
-		delete(sv, resource)
-	}
 }
